6b98ec0e71017e35f0970b0152f98f6fc19dda30
Lucas Redigolo <lucas_redigolo@hotmail.com>
2017-11-22 21:56:21 -0200

    delegates

---
+++ ExemploDelegate1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExemploDelegate1
+{
+    class Program
+    {
+        public delegate void Cortador(string Queijo);
+        static void Main(string[] args)
+        {
+            Cortador delegado = new Cortador(Data);
+            delegado("Ola, eu sou um delegate e ");
+            Cortador delegado2 = new Cortador(DataCompleta);
+            delegado2("Eita peste ");
+        }
+        static void Data(string texto)
+        {
+            System.Console.WriteLine(texto+"hoje e " + DateTime.Now.ToShortDateString());
+        }
+        static void DataCompleta(string nome) 
+        {
+            System.Console.WriteLine("Ola " + nome + " hoje e " + DateTime.Now.ToLongDateString());
+        }
+    }
+}
